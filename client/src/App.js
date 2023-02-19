9278923695314489e0f794a3ea6896b5cc8c2aa5
Alameen8739 <allialameen8739@gmail.com>
2023-02-19 20:59:06 +0100

    blog app

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <header>
+          <a href='#' className='logo'>My Blog</a>
+          <nav>
+            <a href='#'>Login</a>
+            <a href='#'>Register</a>
+          </nav>
+        </header>
+        <div className='post'>
+          <img src='https://techcrunch.com/wp-content/uploads/2023/02/CMC_6266.jpg?w=850&h=492&crop=1' />
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
