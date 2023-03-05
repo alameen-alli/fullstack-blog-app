215d149f4e89da836ae7d561f5bac21c073ce9a4
Alameen8739 <allialameen8739@gmail.com>
2023-03-05 20:32:13 +0100

    almost

--- api/express.js
+++ api/express.js
@@ -3,27 +3,36 @@ const app = express();
 const cors = require("cors");
 const User = require("./models/User");
 const { default: mongoose } = require("mongoose");
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const multer  = require('multer');
+const uploadMiddleware = multer({ dest: 'uploads/' });
+const fs = require('fs');
+const path = require("path");
 
-
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(cookieParser());
 
 const saltRounds = 10;
 const salt = bcrypt.genSaltSync(saltRounds);
-const secretToken = 'jkdnonenrjnenroieern';
-
+const secretToken = "jkdnonenrjnenroieern";
 
 mongoose.connect("mongodb+srv://allialameen8739:zzZcITrGkRdV6YwM@cluster0.hnsov1d.mongodb.net/?retryWrites=true&w=majority");
 
 
+// This route handles the registration process by extracting the username and password from the request body. 
+// It then creates a new user with the hashed password using bcrypt and saves it to the database using the User.create() method. 
+// If successful, it sends a JSON response with the new user details, otherwise it sends a 400 error response.
+
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
   try {
-    const UserDetails = await User.create({username, password:bcrypt.hashSync(password, salt)});
+    const UserDetails = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
     res.json(UserDetails);
   } catch (e) {
     console.log(e);
@@ -31,36 +40,54 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.post('/login', async (req,res) => {
-  const {username, password} = req.body;
-  const UserDetails = await User.findOne({username});
+// It checks the username and password sent in the request body 
+// against the stored user credentials in the database. 
+// If the password matches, it returns a response indicating a successful login. 
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const UserDetails = await User.findOne({ username });
   const passWordOk = bcrypt.compareSync(password, UserDetails.password);
 
   if (passWordOk) {
-    jwt.sign({username, id:UserDetails._id}, secretToken, {}, (err, token) => {
-      if (err) throw err;
-      res.cookie('token', token).json({
-        id:UserDetails._id,
-        username
-      });
-    });
+    jwt.sign(
+      { username, id: UserDetails._id },
+      secretToken,
+      {},
+      (err, token) => {
+        if (err) throw err;
+        res.cookie("token", token).json({
+          id: UserDetails._id,
+          username,
+        });
+      }
+    );
   } else {
-    res.status(400).json('Wrong credentials!');
+    res.status(400).json("Wrong credentials!");
   }
 });
 
-app.get('/profile', (req,res) => {
-  const {token} = req.cookies;
+// it sends a JSON response with the decoded token information in the info object.
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
   jwt.verify(token, secretToken, {}, (err, info) => {
     if (err) throw err;
     res.json(info);
-  })
+  });
 });
 
 // logout endpoint clears the token cookie by setting its value to an empty string
-app.post('/logout', (req, res) => {
-  res.cookie('token', '').json('token cleared');
-})
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json("token cleared");
+});
+
+app.post("/post", uploadMiddleware.single('file'), (req, res) => {
+  const {originalname, path} = req.file;
+  const parts = originalname.split('.');
+  const ext = parts[parts.length - 1];
+  fs.renameSync(path, path + '.' + ext);
+  res.json({ext});
+});
 
 const PORT = 4040;
 app.listen(PORT, function () {
--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,7 +1,65 @@
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
 export default function CreatePost() {
-    return (
-        <div>
-            Create your post
-        </div>
-    )
+
+  const [title, setTitle] = useState('');
+  const [summary, setSummary] = useState('');
+  const [content, setContent] = useState('');
+  const [files, setFiles] = useState('');
+
+ async function createNewPost(e) {
+    const data = new FormData();
+    data.set('title', title);
+    data.set('summary', summary);
+    data.set('content', content);
+    data.set('file', files[0]);
+
+    e.preventDefault();
+    console.log(files)
+
+    const response = await fetch('http://localhost:4040/post', {
+        method: 'POST',
+        body: data
+    })
+    console.log(await response.json());
+  };
+
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      ["link", "image", "video"],
+      ["clean"],
+    ],
+  };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "video",
+  ];
+
+  return (
+    <div>
+      <form onSubmit={createNewPost} >
+        <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <input type="text" placeholder="Summary" value={summary} onChange={(e) => setSummary(e.target.value)} />
+        <input type="file" onChange={(e) => setFiles(e.target.files)} />
+        <ReactQuill value={content} modules={modules} formats={formats} onChange={newValue => setContent(newValue)} />
+        <button className="post">Create Post</button>
+      </form>
+    </div>
+  );
 }
