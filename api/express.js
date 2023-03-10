ec20c9ca1e36f4596d02cd151daf5e1d628cc10e
Alameen8739 <allialameen8739@gmail.com>
2023-03-10 18:27:57 +0100

    edit post page

--- api/express.js
+++ api/express.js
@@ -6,26 +6,27 @@ const { default: mongoose } = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
-const multer  = require('multer');
-const uploadMiddleware = multer({ dest: 'uploads/' });
-const fs = require('fs');
+const multer = require("multer");
+const uploadMiddleware = multer({ dest: "uploads/" });
+const fs = require("fs");
 const path = require("path");
 const Post = require("./models/Post");
 
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(cookieParser());
-app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
 const saltRounds = 10;
 const salt = bcrypt.genSaltSync(saltRounds);
 const secretToken = "jkdnonenrjnenroieern";
 
-mongoose.connect("mongodb://allialameen8739:zzZcITrGkRdV6YwM@ac-kavuqoc-shard-00-00.hnsov1d.mongodb.net:27017,ac-kavuqoc-shard-00-01.hnsov1d.mongodb.net:27017,ac-kavuqoc-shard-00-02.hnsov1d.mongodb.net:27017/?ssl=true&replicaSet=atlas-8cpki0-shard-0&authSource=admin&retryWrites=true&w=majority");
+mongoose.connect(
+  "mongodb://allialameen8739:zzZcITrGkRdV6YwM@ac-kavuqoc-shard-00-00.hnsov1d.mongodb.net:27017,ac-kavuqoc-shard-00-01.hnsov1d.mongodb.net:27017,ac-kavuqoc-shard-00-02.hnsov1d.mongodb.net:27017/?ssl=true&replicaSet=atlas-8cpki0-shard-0&authSource=admin&retryWrites=true&w=majority"
+);
 
-
-// This route handles the registration process by extracting the username and password from the request body. 
-// It then creates a new user with the hashed password using bcrypt and saves it to the database using the User.create() method. 
+// This route handles the registration process by extracting the username and password from the request body.
+// It then creates a new user with the hashed password using bcrypt and saves it to the database using the User.create() method.
 // If successful, it sends a JSON response with the new user details, otherwise it sends a 400 error response.
 
 app.post("/register", async (req, res) => {
@@ -42,9 +43,9 @@ app.post("/register", async (req, res) => {
   }
 });
 
-// It checks the username and password sent in the request body 
-// against the stored user credentials in the database. 
-// If the password matches, it returns a response indicating a successful login. 
+// It checks the username and password sent in the request body
+// against the stored user credentials in the database.
+// If the password matches, it returns a response indicating a successful login.
 
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
@@ -84,37 +85,75 @@ app.post("/logout", (req, res) => {
 });
 
 // This is an Express.js route handler for creating a new blog post. It uses the uploadMiddleware middleware to handle file uploads, renames the uploaded file, and then creates a new Post object with the provided title, summary, content, and image path. Finally, it sends a JSON response with the created postContent.
-app.post("/post", uploadMiddleware.single('file'), async (req, res) => {
-  const {originalname, path} = req.file;
-  const parts = originalname.split('.');
+app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
+  const { originalname, path } = req.file;
+  const parts = originalname.split(".");
   const ext = parts[parts.length - 1];
-  const newPath = path + '.' + ext;
-  fs.renameSync(path, newPath );
+  const newPath = path + "." + ext;
+  fs.renameSync(path, newPath);
 
   const { token } = req.cookies;
   jwt.verify(token, secretToken, {}, async (err, info) => {
     if (err) throw err;
-    const {title, summary, content} = req.body;
+    const { title, summary, content } = req.body;
     const postContent = await Post.create({
       title,
       summary,
       content,
-      image:newPath,
-      author: info.id
+      image: newPath,
+      author: info.id,
     });
     res.json(postContent);
   });
+});
+
+app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
+  let newPath = null;
+  if (req.file) {
+    const { originalname, path } = req.file;
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    newPath = path + "." + ext;
+    fs.renameSync(path, newPath);
+  }
+
+  const { token } = req.cookies;
+  jwt.verify(token, secretToken, {}, async (err, info) => {
+
+    if (err) throw err;
+    const { id, title, summary, content } = req.body;
+    const postContent = await Post.findById(id);
+    const isAuthor = JSON.stringify(postContent.author) === JSON.stringify(info.id)
+    // res.json({postContent, info, isAuthor});
+
+    if (!isAuthor) {
+      return res.status(400).json('You are not the author')
+    }
+
+    await postContent.update({
+      title,
+      summary,
+      content,
+      image: newPath ? newPath : postContent.image
+    });
+
+    res.json(postContent);
   });
+});
 
-  
-
-app.get('/post', async (req, res) => {
+app.get("/post", async (req, res) => {
   const posts = await Post.find()
-  .populate('author', ['username'])
-  .sort({createdAt: -1})
-  .limit(20);
+    .populate("author", ["username"])
+    .sort({ createdAt: -1 })
+    .limit(20);
   res.json(posts);
-})
+});
+
+app.get("/post/:id", async (req, res) => {
+  const { id } = req.params;
+  const postContent = await Post.findById(id).populate("author", ["username"]);
+  res.json(postContent);
+});
 
 const PORT = 4040;
 app.listen(PORT, function () {
---
+++ client/src/pages/EditPost.js
@@ -0,0 +1,102 @@
+import { useEffect, useState } from "react";
+import ReactQuill from "react-quill";
+import { Navigate, useParams } from "react-router-dom";
+import "react-quill/dist/quill.snow.css";
+
+export default function EditPost() {
+  const { id } = useParams();
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
+  const [redirect, setRedirect] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:4040/post/" + id).then((response) => {
+      response.json().then((postInfo) => {
+        setTitle(postInfo.title);
+        setSummary(postInfo.summary);
+        setContent(postInfo.content);
+      });
+    });
+  }, []);
+
+  async function UpdatePost(e) {
+    const data = new FormData();
+    data.set("title", title);
+    data.set("summary", summary);
+    data.set("content", content);
+    data.set("id", id);
+    if (files?.[0]) {
+      data.set("file", files[0]);
+    }
+
+    e.preventDefault();
+    console.log(files);
+
+    const response = await fetch("http://localhost:4040/post", {
+      method: "PUT",
+      body: data,
+      credentials: "include",
+    });
+    if (response.ok) {
+      setRedirect(true);
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={'/post/' +id} />
+  }
+
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      ["link", "image", "video"],
+      ["clean"],
+    ],
+  };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "video",
+  ];
+
+  return (
+    <div>
+      <form onSubmit={UpdatePost}>
+        <input
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Summary"
+          value={summary}
+          onChange={(e) => setSummary(e.target.value)}
+        />
+        <input type="file" onChange={(e) => setFiles(e.target.files)} />
+        <ReactQuill
+          value={content}
+          modules={modules}
+          formats={formats}
+          onChange={(newValue) => setContent(newValue)}
+        />
+        <button className="post">Update Post</button>
+      </form>
+    </div>
+  );
+}
